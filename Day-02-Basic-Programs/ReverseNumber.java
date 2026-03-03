dc7f6a7e9acf38d00bcc8c8c82205c3d93a01511
Amrutha <156908852+Ammuzzamurutha@users.noreply.github.com>
2026-03-03 21:19:08 +0530

    Update ReverseNumber.java

--- Day-02-Basic-Programs/ReverseNumber.java
+++ Day-02-Basic-Programs/ReverseNumber.java
@@ -1,1 +1,18 @@
+package DayTwo;
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int num=sc.nextInt();
+        int reverse=0;
+        while(num!=0){
+            reverse=reverse*10+num%10;
+            num=num/10;
+        }
+        System.out.println("Reverse of the Number" +reverse);
+
+    }
+}
 
